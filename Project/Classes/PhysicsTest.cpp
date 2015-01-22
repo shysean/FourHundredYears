9139edd2328ffcc8c821bc77dd6515389b76ee78
shihongyang <shihongyang0218@gmail.com>
2015-01-22 20:36:04 +0800

    2015-01-22 update physics test

--- Project/Classes/PhysicsTest.cpp
+++ Project/Classes/PhysicsTest.cpp
@@ -22,10 +22,58 @@ PhysicsTest::PhysicsTest()
     
     Size winSize = Director::getInstance()->getWinSize();
     auto body = PhysicsBody::createEdgeBox(winSize, PHYSICSBODY_MATERIAL_DEFAULT, 3);
+    body->setCategoryBitmask(0x00);
+
     auto edgeNode = Node::create();
     edgeNode->setPosition(Point(winSize.width/2,winSize.height/2));
     edgeNode->setPhysicsBody(body);
     this->addChild(edgeNode);
+    
+    auto listener = EventListenerPhysicsContact::create();
+    listener->onContactBegin = [](PhysicsContact& contact)
+    {
+        auto spriteA = (Sprite*)contact.getShapeA()->getBody()->getNode();
+        auto spriteB = (Sprite*)contact.getShapeB()->getBody()->getNode();
+        
+        if (spriteA && spriteA->getTag() == 1 && spriteB && spriteB->getTag() == 1)
+        {
+            spriteA->setColor(Color3B::YELLOW);
+            spriteB->setColor(Color3B::YELLOW);
+        }
+        
+        log("onContactBegin");
+        return true;
+    };
+    
+    
+    listener->onContactPreSolve = [] (PhysicsContact& contact, PhysicsContactPreSolve& solve)
+    {
+        log("onContactPreSolve");
+        return true;
+    };
+    
+    
+    listener->onContactPostSolve = [] (PhysicsContact& contact,const PhysicsContactPostSolve& solve)
+    {
+        log("onContactPostSolve");
+    };
+    
+    
+    listener->onContactSeperate = [](PhysicsContact& contact) {
+        auto spriteA = (Sprite*)contact.getShapeA()->getBody()->getNode();
+        auto spriteB = (Sprite*)contact.getShapeB()->getBody()->getNode();
+        
+        if (spriteA && spriteA->getTag() == 1 && spriteB && spriteB->getTag() == 1)
+        {
+            spriteA->setColor(Color3B::WHITE);
+            spriteB->setColor(Color3B::WHITE);
+        }
+        log("onContactSeperate");
+    };
+    
+    
+    Director::getInstance()->getEventDispatcher()->addEventListenerWithFixedPriority(listener,1);
+    
 }
 
 PhysicsTest::~PhysicsTest()
@@ -74,14 +122,25 @@ void PhysicsTest::testHeroBody()
     m_hero = Hero::create();
     m_hero->playAniByName("walk");
     m_hero->setPosition(Vec2(winSize.width * 0.5, winSize.height * 0.5));
-    this->addChild(m_hero);
     
     auto material = PhysicsMaterial(1.0f, 0.0f, 0.1f);
     auto body = PhysicsBody::createBox(m_hero->getContentSize(), material);
-//    body->setVelocityLimit(10.0f);
-//    body->setDynamic(false);        // staic
-//    body->setResting(false);
+    body->setContactTestBitmask(0x00);
+    body->setCategoryBitmask(0x01);
     m_hero->setPhysicsBody(body);
+    this->addChild(m_hero);
+    
+    auto hero = Hero::create();
+    hero->playAniByName("walk");
+    hero->setPosition(Vec2(winSize.width * 0.5, winSize.height * 0.4));
+    
+    auto material1 = PhysicsMaterial(1.0f, 0.0f, 0.1f);
+    auto body1 = PhysicsBody::createBox(m_hero->getContentSize(), material1);
+    body->setContactTestBitmask(0x10);
+    body->setCategoryBitmask(0x10);
+    hero->setPhysicsBody(body1);
+    this->addChild(hero);
+
 }
 
 void PhysicsTest::testHeroMoveLeft()
